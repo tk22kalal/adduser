08ccdb20a6cb8b0ff5d20ea24f3758097b94228c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 06:46:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
