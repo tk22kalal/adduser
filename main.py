ef87a020fe0b455769ef466a0227e5aa2143cf7b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 07:37:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
+# main.py
+
 from bot import Bot
 
 async def main():
-    # Create an instance of the Bot class
     bot = Bot()
+    await bot.run()
 
-    # Run the add_users method
-    await bot.start()
-    await bot.add_users()
-    await bot.stop()
-
-# Run the main function
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
