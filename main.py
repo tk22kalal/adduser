08d0a62dcf3b94e831d9029a37037aa67044a755
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 07:07:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from bot import Bot
 
-Bot().run()
+# Create an instance of the Bot class
+bot_instance = Bot()
+
+# Run the specific method (e.g., add_users)
+bot_instance.run(bot_instance.add_users)
