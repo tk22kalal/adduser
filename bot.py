4db964547d9b9aee64328a212d5476011f21677c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 06:28:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 from pyrogram.errors import PeerIdInvalid, UserAlreadyParticipant
 
-api_id = '24316517'
-api_hash = 'ab33479d43c662f11cf9ae4b26350709'
-bot_token = '6922414869:AAEZ4iSuI2eTiLwlGDGbT-_h_18951vxgNM'
+your_api_id = '24316517'
+your_api_hash = 'ab33479d43c662f11cf9ae4b26350709'
+your_bot_token = '6922414869:AAEZ4iSuI2eTiLwlGDGbT-_h_18951vxgNM'
 
 # Replace these with the actual numeric IDs of your private channels
 source_channel_id = -1001845192858
@@ -12,37 +12,58 @@ destination_channel_id = -1002121021005
 from pyrogram import Client
 from pyrogram.errors import PeerIdInvalid, UserAlreadyParticipant
 
-app = Client("session_name", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+class Bot:
+    def __init__(self, api_id, api_hash, bot_token, source_channel_id, destination_channel_id):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.bot_token = bot_token
+        self.source_channel_id = source_channel_id
+        self.destination_channel_id = destination_channel_id
 
-async def add_users():
-    async with app:
-        try:
-            # Get information about the source channel
-            source_channel = await app.get_chat(source_channel_id)
+        self.app = Client(
+            "session_name",
+            api_id=self.api_id,
+            api_hash=self.api_hash,
+            bot_token=self.bot_token
+        )
 
-            # Get information about the destination channel
-            destination_channel = await app.get_chat(destination_channel_id)
+    async def add_users(self):
+        async with self.app:
+            try:
+                # Get information about the source channel
+                source_channel = await self.app.get_chat(self.source_channel_id)
 
-            # Check if the bot is an admin in both channels
-            source_admin = await app.get_chat_member(source_channel.id, app.get_me().id)
-            destination_admin = await app.get_chat_member(destination_channel.id, app.get_me().id)
+                # Get information about the destination channel
+                destination_channel = await self.app.get_chat(self.destination_channel_id)
 
-            if source_admin.status == "administrator" and destination_admin.status == "administrator":
-                # Get all the members from the source channel
-                members = await app.get_chat_members(source_channel.id)
+                # Check if the bot is an admin in both channels
+                source_admin = await self.app.get_chat_member(source_channel.id, self.app.get_me().id)
+                destination_admin = await self.app.get_chat_member(destination_channel.id, self.app.get_me().id)
 
-                # Add each member to the destination channel
-                for member in members:
-                    try:
-                        await app.add_chat_members(destination_channel.id, member.user.id)
-                        print(f"Added user {member.user.id} to the destination channel.")
-                    except UserAlreadyParticipant:
-                        print(f"User {member.user.id} is already in the destination channel.")
-                    except Exception as e:
-                        print(f"Error adding user {member.user.id} to the destination channel: {e}")
+                if source_admin.status == "administrator" and destination_admin.status == "administrator":
+                    # Get all the members from the source channel
+                    members = await self.app.get_chat_members(source_channel.id)
 
-        except PeerIdInvalid:
-            print("Invalid source or destination channel ID.")
+                    # Add each member to the destination channel
+                    for member in members:
+                        try:
+                            await self.app.add_chat_members(destination_channel.id, member.user.id)
+                            print(f"Added user {member.user.id} to the destination channel.")
+                        except UserAlreadyParticipant:
+                            print(f"User {member.user.id} is already in the destination channel.")
+                        except Exception as e:
+                            print(f"Error adding user {member.user.id} to the destination channel: {e}")
+
+            except PeerIdInvalid:
+                print("Invalid source or destination channel ID.")
 
 if __name__ == '__main__':
-    app.run(add_users)
+    bot = Bot(
+        api_id='your_api_id',
+        api_hash='your_api_hash',
+        bot_token='your_bot_token',
+        source_channel_id=-1001845192858,
+        destination_channel_id=-1002121021005
+    )
+    bot.app.run(bot.add_users)
+
