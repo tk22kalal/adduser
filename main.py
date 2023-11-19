509c497bb6389060e58769bc541e0499efc9c77e
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 06:36:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from bot import Bot
 
-Bot().run()
+bot = Bot(
+    api_id='24316517',
+    api_hash='ab33479d43c662f11cf9ae4b26350709',
+    bot_token='6922414869:AAEZ4iSuI2eTiLwlGDGbT-_h_18951vxgNM',
+    source_channel_id=-1001845192858,
+    destination_channel_id=-1002121021005
+)
+bot.app.run(bot.add_users)
+
