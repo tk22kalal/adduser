7210032dbe4a68d4808c1443f3999a10d3ea9bd9
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 07:33:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,52 +1,86 @@
-from pyrogram import Client
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from pyrogram.errors import PeerIdInvalid, UserAlreadyParticipant
-from config import API_HASH, API_ID, TG_BOT_TOKEN, SOURCE_CHANNEL_ID, DESTINATION_CHANNEL_ID
+from config import API_HASH, API_ID, TG_BOT_TOKEN
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name="Bot",
+            "Bot",
             api_hash=API_HASH,
             api_id=API_ID,
             bot_token=TG_BOT_TOKEN,
-            # Remove source_channel_id and destination_channel_id from here
         )
-        
-        # Set source_channel_id and destination_channel_id as attributes
-        self.source_channel_id = SOURCE_CHANNEL_ID
-        self.destination_channel_id = DESTINATION_CHANNEL_ID
-        
+        self.source_channel_id = None
+        self.destination_channel_id = None
 
-    async def add_users(self):
-        try:
-            # Get information about the source channel
-            source_channel = await self.get_chat(self.source_channel_id)
+    async def start(self):
+        print("Bot started. Please forward a message from the source channel.")
+        await self.idle()
 
-            # Get information about the destination channel
-            destination_channel = await self.get_chat(self.destination_channel_id)
+    @staticmethod
+    def is_admin(user_id):
+        # Check if the user is an admin (you can modify this logic based on your requirements)
+        return True
 
-            # Check if the bot is an admin in both channels
-            source_admin = await self.get_chat_member(source_channel.id, (await self.get_me()).id)
-            destination_admin = await self.get_chat_member(destination_channel.id, (await self.get_me()).id)
+    async def process_forward(self, message: Message):
+        user_id = message.from_user.id
 
-            if source_admin.status == "administrator" and destination_admin.status == "administrator":
-                # Get all the members from the source channel
-                members = await self.get_chat_members(source_channel.id)
+        if not self.is_admin(user_id):
+            print(f"User {user_id} is not authorized to configure the bot.")
+            return
 
-                # Add each member to the destination channel
-                for member in members:
-                    try:
-                        await self.add_chat_members(destination_channel.id, member.user.id)
-                        print(f"Added user {member.user.id} to the destination channel.")
-                    except UserAlreadyParticipant:
-                        print(f"User {member.user.id} is already in the destination channel.")
-                    except Exception as e:
-                        print(f"Error adding user {member.user.id} to the destination channel: {e}")
+        if not message.forward_from_chat:
+            print("Please forward a message from a channel.")
+            return
 
-        except PeerIdInvalid:
-            print("Invalid source or destination channel ID.")
+        chat_id = message.forward_from_chat.id
+
+        if not self.source_channel_id:
+            print(f"Source Channel ID set to: {chat_id}")
+            self.source_channel_id = chat_id
+            print("Please forward a message from the destination channel.")
+        elif not self.destination_channel_id:
+            print(f"Destination Channel ID set to: {chat_id}")
+            self.destination_channel_id = chat_id
+            print("Configuring complete. Adding users from the source channel to the destination channel.")
+
+            try:
+                # Get information about the source channel
+                source_channel = await self.get_chat(self.source_channel_id)
+
+                # Get information about the destination channel
+                destination_channel = await self.get_chat(self.destination_channel_id)
+
+                # Check if the bot is an admin in both channels
+                source_admin = await self.get_chat_member(source_channel.id, (await self.get_me()).id)
+                destination_admin = await self.get_chat_member(destination_channel.id, (await self.get_me()).id)
+
+                if source_admin.status == "administrator" and destination_admin.status == "administrator":
+                    # Get all the members from the source channel
+                    members = await self.get_chat_members(source_channel.id)
+
+                    # Add each member to the destination channel
+                    for member in members:
+                        try:
+                            await self.add_chat_members(destination_channel.id, member.user.id)
+                            print(f"Added user {member.user.id} to the destination channel.")
+                        except UserAlreadyParticipant:
+                            print(f"User {member.user.id} is already in the destination channel.")
+                        except Exception as e:
+                            print(f"Error adding user {member.user.id} to the destination channel: {e}")
+
+            except PeerIdInvalid:
+                print("Invalid source or destination channel ID.")
+
+            await self.stop()
+        else:
+            print("Both source and destination channels are already configured.")
+
+    async def on_message(self, message: Message):
+        await self.process_forward(message)
 
 
-# Create an instance of the Bot class
-bot = Bot()
-
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run(bot.start)
